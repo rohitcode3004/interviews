3c5cfbbe3d353360449d2f81d4328fc322dfe6a9
rohit@coder <bishtrohit3004@gmail.com>
2020-12-29 21:06:46 +0530

    second commit

---
+++ school/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('', views.apiOverview, name="api-overview"),
+	path('student-list', views.studentList, name="student-list"),
+	path('student-detail/<str:pk>/', views.studentDetail, name="student-detail"),
+	path('student-create/', views.studentCreate, name="student-create"),
+	path('student-update/<str:pk>/', views.studentUpdate, name="student-update"),
+	path('student-delete/<str:pk>/', views.studentDelete, name="student-delete"),
+]
--- school/api/views.py
+++ school/api/views.py
@@ -1,3 +1,63 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import StudentSerializer
+from .models import Student
 
 # Create your views here.
+
+@api_view(['GET'])
+def apiOverview(request):
+	api_urls = {
+		'List': '/task-list',
+		'Detail View': '/task-detail/<str:pk>/',
+		'Create': '/task-create/',
+		'Update': '/task-update/<str:pk>/',
+		'Delete': '/task-delete/<str:pk>/',
+	}
+
+	return Response(api_urls)
+
+
+@api_view(['GET'])
+def studentList(request):
+	students = Student.objects.all()
+	serializer  = StudentSerializer(students, many=True)  #can return multiple objects
+	return Response(serializer.data)
+
+
+@api_view(['GET'])
+def studentDetail(request, pk):
+	student = Student.objects.get(id=pk)
+	serializer  = StudentSerializer(student, many=False)  #can return one object only
+	return Response(serializer.data)
+
+
+@api_view(['POST'])
+def studentCreate(request):
+	serializer  = StudentSerializer(data=request.data)  #get data in json format
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
+
+
+@api_view(['POST'])
+def studentUpdate(request, pk):
+	student = Student.objects.get(id=pk)
+	serializer  = StudentSerializer(instance=student, data=request.data)  #get data in json format
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def studentDelete(request, pk):
+	student = Student.objects.get(id=pk)
+	student.delete()
+
+	return Response("Item successfully deleted!")
